6e2b44a357a64778059ad938b81d0269b1bd1e1c
enricava <enricava@ucm.es>
2024-02-09 17:17:18 +0100

    checkpoint

--- lua/ecavenr/plugins/bufferline.lua
+++ lua/ecavenr/plugins/bufferline.lua
@@ -8,6 +8,7 @@ return {
         config = function()
             require("bufferline").setup()
             local map = require("helpers.keys").map
+            map("n", "<S-x>", "<CMD>bd<CR>", "Close current buffer")
             map("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>", "Next buffer/tab")
             map("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>", "Previous buffer/tab")
         end
