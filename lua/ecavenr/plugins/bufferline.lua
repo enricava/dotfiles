3000e5a2b45d882cccee743e31ea06740726e159
enricava <enricava@ucm.es>
2024-02-09 16:48:22 +0100

    feat: neo-tree + bufferline

--- lua/ecavenr/plugins/bufferline.lua
+++ lua/ecavenr/plugins/bufferline.lua
@@ -6,9 +6,10 @@ return {
 		dependencies = "nvim-tree/nvim-web-devicons",
 		opts = {},
         config = function()
+            require("bufferline").setup()
             local map = require("helpers.keys").map
-            map("n", "<S-h>", "<CMD>BufferLineCycleNext<CR>", "Next buffer/tab")
-            map("n", "<S-l>", "<CMD>BufferLineCyclePrev<CR>", "Previous buffer/tab")
+            map("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>", "Next buffer/tab")
+            map("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>", "Previous buffer/tab")
         end
     },
 }
