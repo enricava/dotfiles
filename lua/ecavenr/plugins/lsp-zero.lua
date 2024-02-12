7df054fb582800fa3417936f80eba547c49cd641
enricava <enricava@ucm.es>
2024-02-12 09:18:16 +0100

    feat: initial lsp config

---
+++ lua/ecavenr/plugins/lsp-zero.lua
@@ -0,0 +1,26 @@
+return {
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        lazy = true,
+        config = false,
+    },
+    {
+        'neovim/nvim-lspconfig',
+        dependencies = {
+            {'hrsh7th/cmp-nvim-lsp'},
+        },
+        config = function()
+            require("ecavenr.lspconfig")
+        end
+    },
+    -- Autocompletion
+    {
+        'hrsh7th/nvim-cmp',
+        dependencies = {
+            {'L3MON4D3/LuaSnip'}
+        },
+    },
+}
