6e09e193ef00e2bba26575ff370f6c1f80a06e3b
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-02-16 13:04:30 +0100

    feat: added telescope mappings
    Change-Id: Id13e542c9e5ef8779214f40dacdc2a98cdd53d37

--- lua/ecavenr/plugins/telescope.lua
+++ lua/ecavenr/plugins/telescope.lua
@@ -4,5 +4,8 @@ return {
         require("telescope").setup()
         local map = require("ecavenr.keys").map
         map("n", "<leader>ff", "<CMD>Telescope find_files<CR>", "Find all files")
+        map("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", "Live grep")
+        map("n", "<leader>fb", "<CMD>Telescope buffers<CR>", "Buffers")
+        map("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", "Help tags")
     end
 }
