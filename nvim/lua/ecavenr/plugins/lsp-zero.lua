c029955bea7edc5ff42c1e25d9f254e574066b44
enricava <enricava@ucm.es>
2024-05-11 00:18:58 +0200

    nvim: rm lsp signature

--- nvim/lua/ecavenr/plugins/lsp-zero.lua
+++ nvim/lua/ecavenr/plugins/lsp-zero.lua
@@ -23,10 +23,10 @@ return {
             { 'L3MON4D3/LuaSnip' }
         },
     },
-    {
-        'ray-x/lsp_signature.nvim',
-        event = "VeryLazy",
-        opts = {},
-        config = function(_, opts) require 'lsp_signature'.setup(opts) end
-    },
+    -- {
+    --     'ray-x/lsp_signature.nvim',
+    --     event = "VeryLazy",
+    --     opts = {},
+    --     config = function(_, opts) require 'lsp_signature'.setup(opts) end
+    -- },
 }
