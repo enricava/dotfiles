fba466af0953462bdfea15d9818e01004ce65cec
enricava <enricava@ucm.es>
2024-05-17 20:19:44 +0200

    nvim: remove Comment.nvim
    not needed in 0.10

--- nvim/lua/ecavenr/plugins/treesitter.lua
+++ nvim/lua/ecavenr/plugins/treesitter.lua
@@ -40,16 +40,6 @@ return {
         opts = {} -- this is equalent to setup({}) function
     },
 
-    -- Commenting, like gcc and gc
-    {
-        "numToStr/Comment.nvim",
-        opts = {},
-        lazy = false,
-        config = function()
-            require("Comment").setup()
-        end
-    },
-
     -- Remove spaces to the right, but only on modified lines
     {
         "thirtythreeforty/lessspace.vim"
