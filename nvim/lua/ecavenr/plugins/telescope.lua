7f17880f02390d1ee275cb493d0499102e4c24e7
enricava <enricava@ucm.es>
2024-05-24 22:41:44 +0200

    nvim: added telescope remap

--- nvim/lua/ecavenr/plugins/telescope.lua
+++ nvim/lua/ecavenr/plugins/telescope.lua
@@ -8,6 +8,7 @@ return {
         map("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", "Live grep")
         map("n", "<leader>fb", "<CMD>Telescope buffers<CR>", "Buffers")
         map("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", "Help tags")
+        map("n", "<leader>fo", "<CMD>Telescope oldfiles<CR>", "Find recent files")
 
         -- LSP remaps
         map("n", "<leader>fr", "<CMD>Telescope lsp_references<CR>", "LSP References")
