b821dbc223d7d57eba80b67d5ae0cf488f5e035d
enricava <enricava@ucm.es>
2024-05-24 22:39:59 +0200

    nvim: update colorscheme config
    better looking telescope window borders, and left default options for
    easy switching

--- nvim/lua/ecavenr/plugins/colorscheme.lua
+++ nvim/lua/ecavenr/plugins/colorscheme.lua
@@ -6,19 +6,34 @@ return {
         lazy = false,    -- load at startup
         priority = 1000, -- before other plugins
         config = function()
-            require("kanagawa").setup({
-                transparent = true,
-                colors = {
-                    theme = {
-                        all = {
-                            ui = {
-                                bg_gutter = "none"
-                            }
-                        }
-                    }
-                }
+            require('kanagawa').setup({
+                compile = false,  -- enable compiling the colorscheme
+                undercurl = true, -- enable undercurls
+                commentStyle = { italic = true },
+                functionStyle = {},
+                keywordStyle = { italic = true },
+                statementStyle = { bold = true },
+                typeStyle = {},
+                transparent = true,    -- do not set background color
+                dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+                terminalColors = true, -- define vim.g.terminal_color_{0,17}
+                colors = {             -- add/modify theme and palette colors
+                    palette = {},
+                    theme = { wave = {}, lotus = {}, dragon = {}, all = { ui = { bg_gutter = "none" } } },
+                },
+                theme = "wave",    -- Load "wave" theme when 'background' option is not set
+                background = {     -- map the value of 'background' option to a theme
+                    dark = "wave", -- try "dragon" !
+                    light = "lotus"
+                },
             })
+
+            -- setup must be called before loading
             vim.cmd([[colorscheme kanagawa-wave]])
+
+            -- fix ugly telescope borders
+            vim.cmd 'highlight TelescopeBorder guibg=none'
+            vim.cmd 'highlight TelescopeTitle guibg=none'
         end,
     },
 }
