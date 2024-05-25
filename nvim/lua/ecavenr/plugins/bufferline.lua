9dd2e08494d7e4ba8b3796cdcf88dd89c0392362
enricava <enricava@ucm.es>
2024-05-25 11:28:20 +0200

    formatting

--- nvim/lua/ecavenr/plugins/bufferline.lua
+++ nvim/lua/ecavenr/plugins/bufferline.lua
@@ -1,35 +1,35 @@
 return {
-    -- See current buffers at the top of the editor
-    {
-        "akinsho/bufferline.nvim",
-        version = "v4.*",
-        dependencies = "nvim-tree/nvim-web-devicons",
-        opts = {},
-        config = function()
-            require("bufferline").setup()
-            local map = require("ecavenr.keys").map
-            map("n", "<S-x>", "<CMD>bd<CR>", "Close current buffer")
-            map("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>", "Next buffer/tab")
-            map("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>", "Previous buffer/tab")
-        end
-    },
+  -- See current buffers at the top of the editor
+  {
+    "akinsho/bufferline.nvim",
+    version = "v4.*",
+    dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {},
+    config = function()
+      require("bufferline").setup()
+      local map = require("ecavenr.keys").map
+      map("n", "<S-x>", "<CMD>bd<CR>", "Close current buffer")
+      map("n", "<S-l>", "<CMD>BufferLineCycleNext<CR>", "Next buffer/tab")
+      map("n", "<S-h>", "<CMD>BufferLineCyclePrev<CR>", "Previous buffer/tab")
+    end
+  },
 
-    -- Highlight similar words and current line
-    {
-        "yamatsum/nvim-cursorline",
-        config = function()
-            require("nvim-cursorline").setup {
-                cursorline = {
-                    enable = true,
-                    timeout = 1000,
-                    number = false,
-                },
-                cursorword = {
-                    enable = true,
-                    min_length = 3,
-                    hl = { underline = true },
-                }
-            }
-        end
-    }
+  -- Highlight similar words and current line
+  {
+    "yamatsum/nvim-cursorline",
+    config = function()
+      require("nvim-cursorline").setup {
+        cursorline = {
+          enable = true,
+          timeout = 1000,
+          number = false,
+        },
+        cursorword = {
+          enable = true,
+          min_length = 3,
+          hl = { underline = true },
+        }
+      }
+    end
+  }
 }
