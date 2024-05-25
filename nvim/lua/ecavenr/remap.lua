49b3a5b571a9524c4fb34be93d6d352c670e5926
enricava <enricava@ucm.es>
2024-05-25 11:18:13 +0200

    nvim: save/exit remaps

--- nvim/lua/ecavenr/remap.lua
+++ nvim/lua/ecavenr/remap.lua
@@ -5,7 +5,18 @@ local map = require("ecavenr.keys").map
 
 map("n", "<leader>nh", "<cmd>:noh<cr>", "Remove search highlight")
 
+-- Window switching
 map("n", "<C-h>", "<C-W>h", "Switch window left")
 map("n", "<C-j>", "<C-W>j", "Switch window down")
 map("n", "<C-k>", "<C-W>k", "Switch window up")
 map("n", "<C-l>", "<C-W>l", "Switch window right")
+
+-- Select all with Ctrl+A
+map("n", "<C-a>", "gg<S-v>G", "Select all")
+
+-- Save with Ctrl+S
+map("n", "<C-s>", ":w<CR>", "Save file")
+map("i", "<C-s>", "<Esc>:w<CR>", "Save file")
+
+-- Exit with Alt+4
+map("n", "<A-4>", ":q<CR>", "Exit vim")
