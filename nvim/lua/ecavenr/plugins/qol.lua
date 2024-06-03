a682b72084ce8595656fb55f6f614091520db8b8
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-06-03 17:01:54 +0200

    nvim: add nvim-surround plugin
    Change-Id: I97d9db2a09f97f25644bfa97259b181d01a5c0f1

--- nvim/lua/ecavenr/plugins/qol.lua
+++ nvim/lua/ecavenr/plugins/qol.lua
@@ -10,4 +10,16 @@ return {
   {
     "thirtythreeforty/lessspace.vim"
   },
+
+  -- Surround text easily
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            -- Configuration here, or leave empty to use defaults
+        })
+    end
+}
 }
