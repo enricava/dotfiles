54279edc8ab0b57eabb392676f5bc76252b95a23
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-06-06 11:48:48 +0200

    nvim: fix trouble
    Change-Id: Idf7157def151dacf667851a9b1ae6c3fb16050f8

--- nvim/lua/ecavenr/plugins/diagnostics.lua
+++ nvim/lua/ecavenr/plugins/diagnostics.lua
@@ -1,12 +1,31 @@
 -- Show diagnostics in new window
 return {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        -- default settings
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    -- default settings
+  },
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>td",
+      "<cmd>Trouble diagnostics toggle focus=true<cr>",
+      desc = "Toggle trouble diagnostics",
     },
-    config = function()
-        local map = require("ecavenr.keys").map
-        map("n", "<leader>tt", "<cmd>TroubleToggle<cr>", "Toggle trouble report")
-    end
+    {
+      "<leader>ts",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Toggle trouble symbols",
+    },
+    {
+      "<leader>tl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "Toggle trouble lsp",
+    },
+    {
+      "<leader>ta",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Toggle trouble quickfix actions",
+    },
+  },
 }
