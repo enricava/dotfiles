a9dbcf43c25887ef99d20e3e5117c79e61a38e94
Enrique C <enricava@ucm.es>
2024-06-09 16:42:46 +0200

    nvim: (neo-tree) floating window and show hidden

--- nvim/lua/ecavenr/plugins/neo-tree.lua
+++ nvim/lua/ecavenr/plugins/neo-tree.lua
@@ -1,33 +1,40 @@
 return {
-    -- "P" to preview files
-    "nvim-lua/plenary.nvim",
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v2.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons",
-            "MunifTanjim/nui.nvim",
-        },
-        config = function()
-            require("neo-tree").setup({
-                window = {
-                    position = "right"
-                },
-            })
-            local map = require("ecavenr.keys").map
-            map(
-                { "n", "v" },
-                "<leader>ee",
-                function()
-                    require("neo-tree.command").execute({
-                        toggle = true,
-                        position = "right",
-                    })
-                end,
-                "Toggle file explorer"
-            )
-        end,
+  -- "P" to preview files
+  "nvim-lua/plenary.nvim",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
     },
-    --   {"akinsho/toggleterm.nvim", version = "*", config = true}
+    config = function()
+      require("neo-tree").setup({
+        close_if_last_window = true,
+        popup_border_style = "rounded",
+        window = {
+          position = "float"
+        },
+        filesystem = {
+          filtered_items = {
+            visible = true,
+          },
+        },
+      })
+      local map = require("ecavenr.keys").map
+      map(
+        { "n", "v" },
+        "<leader>ee",
+        function()
+          require("neo-tree.command").execute({
+            toggle = true,
+            position = "float",
+          })
+        end,
+        "Show file explorer"
+      )
+    end,
+  },
+  --   {"akinsho/toggleterm.nvim", version = "*", config = true}
 }
