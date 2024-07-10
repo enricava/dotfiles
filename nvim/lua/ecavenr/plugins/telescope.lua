53d4f2ca696ebb38a557dd87cb3694baee95a8ea
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-07-10 14:45:43 +0200

    nvim: upgrade telescope
    Change-Id: I488fa53dc36c2c9c4beaa5a3823a1a9b648f04c6

--- nvim/lua/ecavenr/plugins/telescope.lua
+++ nvim/lua/ecavenr/plugins/telescope.lua
@@ -1,8 +1,20 @@
 return {
+  { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
   {
     "nvim-telescope/telescope.nvim",
     config = function()
-      require("telescope").setup()
+      require("telescope").setup({
+        extensions = {
+          fzf = {
+            fuzzy = true,             -- false will only do exact matching
+            override_generic_sorter = true, -- override the generic sorter
+            override_file_sorter = true, -- override the file sorter
+            case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+            -- the default case_mode is "smart_case"
+          }
+        }
+      })
+
       local map = require("ecavenr.keys").map
       -- Telescope mappings
       map("n", "<leader>ff", "<CMD>Telescope find_files<CR>", "Find all files")
@@ -15,8 +27,9 @@ return {
       map("n", "<leader>fr", "<CMD>Telescope lsp_references<CR>", "LSP References")
       map("n", "<leader>fd", "<CMD>Telescope lsp_definitions<CR>", "LSP Definitions")
       map("n", "<leader>fw", "<CMD>Telescope lsp_workspace_symbols<CR>", "LSP Workspace symbols")
-    end
 
+      require('telescope').load_extension('fzf')
+    end
   },
 
   {
