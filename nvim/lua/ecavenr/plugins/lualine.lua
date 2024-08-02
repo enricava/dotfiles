b03b5e71f9b87e527de03348cf47419f4642d3b3
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-08-02 13:25:14 +0200

    nvim: (lualine) add filename to winbar
    Change-Id: I20e989c5e950905792a4cbc407aa2de72a54d781

--- nvim/lua/ecavenr/plugins/lualine.lua
+++ nvim/lua/ecavenr/plugins/lualine.lua
@@ -6,8 +6,8 @@ return {
             options = {
                 icons_enabled = true,
                 theme = "auto",
-                component_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
+                component_separators = { left = "", right = "" },
+                section_separators = { left = "", right = "" },
                 disabled_filetypes = {
                     statusline = {},
                     winbar = {},
@@ -22,7 +22,7 @@ return {
                 }
             },
             sections = {
-                lualine_a = { "mode" },
+                lualine_a = { {"mode", separator = { left = '', right = '' }} },
                 lualine_b = { "branch", "diff", "diagnostics" },
                 lualine_c = { { "filename", path = 1 } },
                 lualine_x = { "encoding", "fileformat", "filetype" },
@@ -38,8 +38,24 @@ return {
                 lualine_z = {}
             },
             tabline = {},
-            winbar = {},
-            inactive_winbar = {},
+
+            winbar = {
+              lualine_a = { {"filename", separator = { left = ''}} },
+              lualine_b = { {"filetype", separator = {} }, {"progress", separator = { right = ''}}},
+              lualine_c = { },
+              lualine_x = {},
+              lualine_y = {},
+              lualine_z = {}
+            },
+
+            inactive_winbar = {
+              lualine_a = { "filename" },
+              lualine_b = { "filetype"},
+              lualine_c = {},
+              lualine_x = {},
+              lualine_y = {},
+              lualine_z = {}
+            },
             extensions = {}
         }
     end
