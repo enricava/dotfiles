13c5908cb56807ec146b016763a72e91cdfa7a05
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-08-02 13:26:20 +0200

    nvim: (dropbar) disable
    Change-Id: I35d06e667279dc68b236b366f819b1a6af51f6d3

--- nvim/lua/ecavenr/plugins/dropbar.lua
+++ nvim/lua/ecavenr/plugins/dropbar.lua
@@ -1,10 +1,10 @@
--- Top dropbar, requires nvim 0.10
 return {
-    {
-        "Bekaboo/dropbar.nvim",
-        -- optional, but required for fuzzy finder support
-        dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim"
-        }
-    },
+--     {
+--         Top dropbar, requires nvim 0.10
+--         "Bekaboo/dropbar.nvim",
+--         -- optional, but required for fuzzy finder support
+--         dependencies = {
+--             "nvim-telescope/telescope-fzf-native.nvim"
+--         }
+--     },
 }
