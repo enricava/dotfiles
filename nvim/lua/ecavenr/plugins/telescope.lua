4d779940446e46fd03d0076aaa548e9b0f703346
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-08-02 13:27:58 +0200

    nvim: (telescope) ripgrep with args
    Change-Id: I9d37a2ace74b8a85d0eef9d9fccfd67b90ba6e92

--- nvim/lua/ecavenr/plugins/telescope.lua
+++ nvim/lua/ecavenr/plugins/telescope.lua
@@ -2,6 +2,11 @@ return {
   { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release' },
   {
     "nvim-telescope/telescope.nvim",
+
+    dependencies = {
+      "nvim-telescope/telescope-live-grep-args.nvim" ,
+    },
+
     config = function()
       require("telescope").setup({
         extensions = {
@@ -16,9 +21,10 @@ return {
       })
 
       local map = require("ecavenr.keys").map
+
       -- Telescope mappings
       map("n", "<leader>ff", "<CMD>Telescope find_files<CR>", "Find all files")
-      map("n", "<leader>fg", "<CMD>Telescope live_grep<CR>", "Live grep")
+      map("n", "<leader>fg", "<CMD>Telescope live_grep_args live_grep_args<CR>", "Live grep (with args)")
       map("n", "<leader>fb", "<CMD>Telescope buffers<CR>", "Buffers")
       map("n", "<leader>fh", "<CMD>Telescope help_tags<CR>", "Help tags")
       map("n", "<leader>fo", "<CMD>Telescope oldfiles<CR>", "Find recent files")
@@ -28,7 +34,9 @@ return {
       map("n", "<leader>fd", "<CMD>Telescope lsp_definitions<CR>", "LSP Definitions")
       map("n", "<leader>fw", "<CMD>Telescope lsp_workspace_symbols<CR>", "LSP Workspace symbols")
 
+      -- Telescope extensions
       require('telescope').load_extension('fzf')
+      require('telescope').load_extension('live_grep_args')
     end
   },
 
