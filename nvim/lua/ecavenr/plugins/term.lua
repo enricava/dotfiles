edfab6364b84ce5c3036e71313536348e9cb8a5d
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-09-25 19:57:23 +0200

    nvim: add ToggleTerm
    Change-Id: Ib7c1d0dff6ef85157b3dd2a9dd75bbdc672854fa

---
+++ nvim/lua/ecavenr/plugins/term.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = function()
+      require('toggleterm').setup({
+      })
+      local map = require("ecavenr.keys").map
+
+      map("n", "<C-w>t", "<CMD>ToggleTerm size=80 direction=vertical<CR>", "Toggle terminal")
+      map("n", "<C-f>", "<CMD>ToggleTerm size=160 direction=float<CR>", "Toggle floating terminal")
+      map("t", "<C-f>", "<CMD>ToggleTerm<CR>", "Toggle floating terminal")
+      map("t", "<C-w>t", "<C-\\><C-n>", "Unfocus terminal")
+    end
+  }
+}
