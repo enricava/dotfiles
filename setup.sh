08155ab9235e0f6fc0e7c7831a42bfe02384b600
Enrique C <enricava@ucm.es>
2024-10-15 21:36:35 +0200

    yazi: initial config

--- setup.sh
+++ setup.sh
@@ -4,3 +4,4 @@ ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/.mybashrc ~/.mybashrc
 ln -s ~/dotfiles/.wezterm.lua ~/.wezterm.lua
+ln -s ~/dotfiles/yazi ~/.config/yazi
