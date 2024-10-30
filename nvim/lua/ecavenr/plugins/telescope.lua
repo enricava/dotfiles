ede2505843c47e56003bfbbbb1fa0f5e2bf89e12
Enrique C <enricava@ucm.es>
2024-10-30 10:55:32 +0100

    eric: revert flutter related commits
    This reverts commit 5a9b508657c7d20f295202db160e24641b41c876.
    
    This reverts commit 10b42a8ed0a9231731049b0b4c3e44ba4ad6820d.

--- nvim/lua/ecavenr/plugins/telescope.lua
+++ nvim/lua/ecavenr/plugins/telescope.lua
@@ -65,7 +65,6 @@ return {
       -- Telescope extensions
       require('telescope').load_extension('fzf')
       require('telescope').load_extension('live_grep_args')
-      require('telescope').load_extension('flutter')
     end
   },
 
