d7939583fcf827765553dcb2f26d2a39af627501
Enrique C <enricava@ucm.es>
2024-11-25 23:50:42 +0100

    nvim: (lualine) add @recording

--- nvim/lua/ecavenr/plugins/lualine.lua
+++ nvim/lua/ecavenr/plugins/lualine.lua
@@ -1,67 +1,71 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-        require("lualine").setup {
-            options = {
-                icons_enabled = true,
-                theme = "auto",
-                component_separators = { left = "", right = "" },
-                section_separators = { left = "", right = "" },
-                disabled_filetypes = {
-                    statusline = {},
-                    winbar = {
-                        -- Do not mess with dap ui
-                        "dapui_watches", "dapui_breakpoints",
-                        "dapui_scopes", "dapui_console",
-                        "dapui_stacks", "dap-repl"
-                    },
-                },
-                ignore_focus = {},
-                always_divide_middle = true,
-                globalstatus = false,
-                refresh = {
-                    statusline = 1000,
-                    tabline = 1000,
-                    winbar = 1000,
-                }
-            },
-            sections = {
-                lualine_a = { {"mode", separator = { left = '', right = '' }} },
-                lualine_b = { "branch", "diff", "diagnostics" },
-                lualine_c = { { "filename", path = 1 } },
-                lualine_x = { "encoding", "fileformat", "filetype" },
-                lualine_y = { "progress" },
-                lualine_z = { "location" }
-            },
-            inactive_sections = {
-                lualine_a = {},
-                lualine_b = {},
-                lualine_c = { "filename" },
-                lualine_x = { "location" },
-                lualine_y = {},
-                lualine_z = {}
-            },
-            tabline = {},
-
-            winbar = {
-              lualine_a = {},
-              lualine_b = {},
-              lualine_c = {},
-              lualine_x = {},
-              lualine_y = {},
-              lualine_z = {}
-            },
-
-            inactive_winbar = {
-              lualine_a = {},
-              lualine_b = {},
-              lualine_c = {},
-              lualine_x = {},
-              lualine_y = {},
-              lualine_z = {}
-            },
-            extensions = {}
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup {
+      options = {
+        icons_enabled = true,
+        theme = "auto",
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {
+            -- Do not mess with dap ui
+            "dapui_watches", "dapui_breakpoints",
+            "dapui_scopes", "dapui_console",
+            "dapui_stacks", "dap-repl"
+          },
+        },
+        ignore_focus = {},
+        always_divide_middle = true,
+        globalstatus = false,
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
         }
-    end
+      },
+      sections = {
+        lualine_a = { { "mode", separator = { left = '', right = '' } } },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = { {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        }, "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" }
+      },
+      inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = { "filename" },
+        lualine_x = { "location" },
+        lualine_y = {},
+        lualine_z = {}
+      },
+      tabline = {},
+
+      winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+      },
+
+      inactive_winbar = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {}
+      },
+      extensions = {}
+    }
+  end
 }
