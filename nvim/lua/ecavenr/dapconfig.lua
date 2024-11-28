f8eb29fd93f1f2bc318a4d5fbfc78c1a2498d626
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2024-11-28 09:57:41 +0100

    nvim: (dap) debug c/c++ through docker
    Change-Id: Ie2922b23ee48da342f52e7a935b07f08255bfbad

--- nvim/lua/ecavenr/dapconfig.lua
+++ nvim/lua/ecavenr/dapconfig.lua
@@ -8,7 +8,67 @@ map("n", "<leader>dc", "<cmd>DapContinue<cr>", "Start/continue debugging")
 map("n", "<leader>di", "<cmd>DapStepInto<cr>", "Step into")
 map("n", "<leader>do", "<cmd>DapStepOver<cr>", "Step over")
 
+-- IO helpers
+local function cmd_read(cmd)
+  local handle = io.popen(cmd)
+  local output = handle:read("*a")
+  handle:close()
+
+  output = output:gsub("%s+", "")
+  return output
+end
+
+local function true_false(prompt)
+  return function()
+    local choices = {
+      prompt,
+      '1. yes',
+      '2. no',
+    }
+
+    return vim.fn.inputlist(choices) == 1
+  end
+end
+
+-- Telescope helpers
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local conf = require("telescope.config").values
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+local binary_finder = finders.new_oneshot_job({ "fd", "--hidden", "--no-ignore", "--type", "x" }, {})
+local image_finder = finders.new_oneshot_job({ "docker", "images", "--format", "{{.Repository}}:{{.Tag}}" }, {})
+
+local function telescoper(prompt, finder)
+  return function()
+    return coroutine.create(function(coro)
+      local opts = {}
+      pickers
+          .new(opts, {
+            prompt_title = prompt,
+            finder = finder,
+            sorter = conf.generic_sorter(opts),
+            attach_mappings = function(buffer_number)
+              actions.select_default:replace(function()
+                actions.close(buffer_number)
+                coroutine.resume(coro, action_state.get_selected_entry()[1])
+              end)
+              return true
+            end,
+          })
+          :find()
+    end)
+  end
+end
+
 -- Debug adapters
+dap.adapters.gdb = {
+  type = 'executable',
+  command = 'gdb',
+  args = { '--interpreter=dap', '--eval-command', 'set print pretty on' }
+}
+
 dap.adapters.dart = {
   type = "executable",
   command = "flutter",
@@ -16,6 +76,53 @@ dap.adapters.dart = {
 }
 
 -- Debug configurations
+local docker_gdb = {
+  name = '󰡨 Debug through docker',
+  type = 'cppdbg',
+  request = 'launch',
+  MIMode = 'gdb',
+  cwd = '${workspaceFolder}',
+  stopAtEntry = true_false(' Stop at entry?'),
+
+  program = telescoper(' Select binary', binary_finder),
+
+  args = function()
+    return vim.split(vim.fn.input(' Args?'), ' ')
+  end,
+
+  sourceFileMap = {
+    ['.'] = '${workspaceFolder}',
+  },
+
+  environment = {
+    {
+      name = 'WS_ROOT',
+      value = '${workspaceFolder}'
+    },
+  },
+
+  pipeTransport = {
+    debuggerPath = '/usr/bin/gdb',
+    pipeProgram = 'docker',
+    pipeCwd = '${workspaceFolder}',
+    pipeArgs = {
+      'run', '-i', '--rm', '--init',
+      '--network', 'none',
+      '--user', cmd_read('id -u') .. ':' .. cmd_read('id -g'),
+      '--cap-add=SYS_PTRACE',
+      '--security-opt', 'seccomp=unconfined',
+      '--volume', '${workspaceFolder}:${workspaceFolder}:rw',
+      '--workdir', '${workspaceFolder}',
+      telescoper('󰡨 Select docker image', image_finder),
+      'bash', '-ec',
+    },
+  },
+}
+
+dap.configurations.cpp = { docker_gdb, }
+
+dap.configurations.c = { docker_gdb, }
+
 dap.configurations.dart = {
   {
     type = "dart",
--- nvim/lua/ecavenr/plugins/dap.lua
+++ nvim/lua/ecavenr/plugins/dap.lua
@@ -1,48 +1,47 @@
-
 return {
-    -- Debug Adapter
-    {
-        "mfussenegger/nvim-dap",
-        config = function()
-            require('ecavenr.dapconfig')
-        end
+  -- Debug Adapter
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require('ecavenr.dapconfig')
+    end
+  },
+
+  -- Close gap between mason and mason-nvim-dap
+  -- cpptools works well
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
     },
-
-    -- Close gap between mason and mason-nvim-dap
-    -- cpptools works well
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        event = "VeryLazy",
-        dependencies = {
-            "williamboman/mason.nvim",
-            "mfussenegger/nvim-dap",
-        },
-        opts = {
-            handlers = {},
-        },
+    opts = {
+      handlers = {},
     },
+  },
 
-    -- Ui for nvim-dap
-    {
-        "rcarriga/nvim-dap-ui",
-        event = "VeryLazy",
-        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
-        config = function()
-            local dap = require("dap")
-            local dapui = require("dapui")
-            dapui.setup()
-            dap.listeners.after.event_initialized["dapui_config"] = function()
-                dapui.open()
-            end
-            dap.listeners.before.event_terminated["dapui_config"] = function()
-                dapui.close()
-            end
-            dap.listeners.before.event_exited["dapui_config"] = function()
-                dapui.close()
-            end
+  -- Ui for nvim-dap
+  {
+    "rcarriga/nvim-dap-ui",
+    event = "VeryLazy",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
 
-            local map = require("ecavenr.keys").map
-            map("n", "<leader>dk", "<cmd>lua require('dapui').eval()<cr>", "Eval value")
-        end
-    }
+      local map = require("ecavenr.keys").map
+      map("n", "<leader>dk", "<cmd>lua require('dapui').eval()<cr>", "Eval value")
+    end
+  }
 }
