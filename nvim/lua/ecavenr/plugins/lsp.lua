e8afa8db316e9018f40d8948dfb83b9abb35a17f
enricava <enricava@ucm.es>
2024-11-29 09:51:39 +0100

    nvim: (lsp) add lua dev plugin

--- nvim/lua/ecavenr/plugins/lsp.lua
+++ nvim/lua/ecavenr/plugins/lsp.lua
@@ -34,7 +34,19 @@ return {
         config = function()
             require('crates').setup()
         end,
-    }
+    },
+    -- Lua development
+    {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+            library = {
+                -- See the configuration section for more details
+                -- Load luvit types when the `vim.uv` word is found
+                { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+            },
+        },
+    },
     -- Rust tools / LSP
     -- {
     --     'mrcjkb/rustaceanvim',
