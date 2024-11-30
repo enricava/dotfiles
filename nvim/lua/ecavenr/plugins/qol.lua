f20fe360615e222b7efe7859362ccdd647ce9d4c
Enrique Cavanillas <enricava@ucm.es>
2024-11-30 13:54:04 +0100

    nvim: (ibl) bump version

--- nvim/lua/ecavenr/plugins/qol.lua
+++ nvim/lua/ecavenr/plugins/qol.lua
@@ -1,37 +1,56 @@
 return {
   -- Automatic pair closing
   {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
     opts = {} -- this is equalent to setup({}) function
   },
 
   -- Remove spaces to the right, but only on modified lines
   {
-    "thirtythreeforty/lessspace.vim"
+    'thirtythreeforty/lessspace.vim'
   },
 
   -- Surround text easily
   {
-    "kylechui/nvim-surround",
-    version = "*", -- Use for stability; omit to use `main` branch for the latest features
-    event = "VeryLazy",
+    'kylechui/nvim-surround',
+    version = '*', -- Use for stability; omit to use `main` branch for the latest features
+    event = 'VeryLazy',
   },
 
   -- Better marks
   {
-    "chentoast/marks.nvim",
-    event = "VeryLazy",
+    'chentoast/marks.nvim',
+    event = 'VeryLazy',
     opts = {},
   },
 
   -- Vertical lines on indents
   {
-    "lukas-reineke/indent-blankline.nvim",
-    main = "ibl",
-    event = "BufEnter",
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    event = 'BufEnter',
     opts = {
-      indent = { char = "|" },
+      indent = {
+        char = '│',
+      },
+      exclude = {
+        filetypes = {
+          'terminal',
+          'nofile',
+          'quickfix',
+          'prompt',
+          'neo-tree',
+          'dashboard',
+          'lspinfo',
+          'help',
+          'man',
+          'gitcommit',
+          'TelescopePrompt',
+          'TelescopeResults',
+          '',
+        },
+      },
     },
   },
 
