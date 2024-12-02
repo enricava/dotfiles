be93fff093254291c3370c062a8249d0051a6b02
enricava <enricava@ucm.es>
2024-12-02 15:53:49 +0100

    nvim: (grug-far) no call to setup

--- nvim/lua/ecavenr/plugins/grug-far.lua
+++ nvim/lua/ecavenr/plugins/grug-far.lua
@@ -1,10 +1,8 @@
+local map = require("ecavenr.keys").map
+map("n", "<leader>fm", "<CMD>GrugFar<CR>", "Find and replace")
+
 return {
   'MagicDuck/grug-far.nvim',
   event = "VeryLazy",
-  config = function()
-    require('grug-far').setup({});
-
-    local map = require("ecavenr.keys").map
-    map("n", "<leader>fm", "<CMD>GrugFar<CR>", "Find and replace")
-  end
+  opts = {}
 }
