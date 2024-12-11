48823ea26d8de4dfcffafdd785c13acee97a86b6
Enrique C <enricava@ucm.es>
2024-12-11 23:38:40 +0100

    fish: dart paths

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,7 @@ fish_add_path /usr/local/android-studio/bin
 fish_add_path /usr/bin/flutter/bin
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.pub-cache/bin
 
 set -x N_PREFIX "$HOME/n"; contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin"  # Added by n-install (see http://git.io/n-install-repo).
 
