3baf9877d25ed59cd06233f997be86f25436584d
Enrique C <enricava@ucm.es>
2024-12-17 19:49:47 +0100

    wezterm: fixed window on font resize

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,6 +6,8 @@ local config = wezterm.config_builder()
 
 config.hide_tab_bar_if_only_one_tab = true
 
+config.adjust_window_size_when_changing_font_size = false
+
 config.window_background_opacity = 1.0
 
 config.window_padding = {
