0c6706a488f4a1da3859047202c5c7da56b8adaf
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2025-01-14 12:34:09 +0100

    nvim: (colorscheme) add nightfox
    Change-Id: I205409c8db655f2f7075ef2eefd61fe787980949

--- nvim/lua/ecavenr/plugins/colorscheme.lua
+++ nvim/lua/ecavenr/plugins/colorscheme.lua
@@ -1,4 +1,51 @@
 return {
+    {
+        "EdenEast/nightfox.nvim",
+        name = "nightfox",
+        lazy = false,
+        priority = 1000, -- before other plugins
+        config = function()
+            -- Default options
+            require('nightfox').setup({
+                options = {
+                    transparent = false, -- Disable setting background
+                    terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+                    dim_inactive = false, -- Non focused panes set to alternative background
+                    module_default = true, -- Default enable value for modules
+                    styles = { -- Style to be applied to different syntax groups
+                        comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+                        conditionals = "NONE",
+                        constants = "NONE",
+                        functions = "NONE",
+                        keywords = "NONE",
+                        numbers = "NONE",
+                        operators = "NONE",
+                        strings = "NONE",
+                        types = "bold",
+                        variables = "NONE",
+                    },
+                    inverse = { -- Inverse highlight for different types
+                        match_paren = false,
+                        visual = false,
+                        search = false,
+                    },
+                    modules = { -- List of various plugins and additional options
+                        -- ...
+                    },
+                },
+                palettes = {},
+                specs = {},
+                groups = {},
+            })
+
+            -- setup must be called before loading
+            vim.cmd("colorscheme nightfox")
+
+            -- fix ugly telescope borders
+            vim.cmd 'highlight TelescopeBorder guibg=none'
+            vim.cmd 'highlight TelescopeTitle guibg=none'
+        end,
+    },
     -- Favorite theme
     {
         "rebelot/kanagawa.nvim",
@@ -53,7 +100,7 @@ return {
             })
 
             -- setup must be called before loading
-            vim.cmd([[colorscheme kanagawa-wave]])
+            -- vim.cmd([[colorscheme kanagawa-wave]])
 
             -- fix ugly telescope borders
             vim.cmd 'highlight TelescopeBorder guibg=none'
