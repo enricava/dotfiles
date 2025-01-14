73e7ae530ef67998959c80ce23a4f3d67d833836
Enrique Cavanillas <enrique.cavanillas@ericsson.com>
2025-01-14 12:26:58 +0100

    nvim: format colorscheme.lua
    Change-Id: Iebf3f948abd6b6d30ad18a6b9ed2fb01e6fbe74f

--- nvim/lua/ecavenr/plugins/colorscheme.lua
+++ nvim/lua/ecavenr/plugins/colorscheme.lua
@@ -14,7 +14,7 @@ return {
                 keywordStyle = { italic = true },
                 statementStyle = { bold = true },
                 typeStyle = {},
-                transparent = false,    -- do not set background color
+                transparent = false,   -- do not set background color
                 dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
                 terminalColors = true, -- define vim.g.terminal_color_{0,17}
                 colors = {             -- add/modify theme and palette colors
@@ -27,29 +27,29 @@ return {
                     light = "lotus"
                 },
                 overrides = function(colors)
-                  local theme = colors.theme
-                  return {
-                      NormalFloat = { bg = "none" },
-                      FloatBorder = { bg = "none" },
-                      FloatTitle = { bg = "none" },
+                    local theme = colors.theme
+                    return {
+                        NormalFloat = { bg = "none" },
+                        FloatBorder = { bg = "none" },
+                        FloatTitle = { bg = "none" },
 
-                      -- Save an hlgroup with dark background and dimmed foreground
-                      -- so that you can use it where your still want darker windows.
-                      -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
-                      NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+                        -- Save an hlgroup with dark background and dimmed foreground
+                        -- so that you can use it where your still want darker windows.
+                        -- E.g.: autocmd TermOpen * setlocal winhighlight=Normal:NormalDark
+                        NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
 
-                      -- Popular plugins that open floats will link to NormalFloat by default;
-                      -- set their background accordingly if you wish to keep them dark and borderless
-                      LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                      MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        -- Popular plugins that open floats will link to NormalFloat by default;
+                        -- set their background accordingly if you wish to keep them dark and borderless
+                        LazyNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        MasonNormal = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
 
-                      Pmenu = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
-                      -- PmenuSel = { bg = theme.ui.bg_m1, fg = theme.ui.fg },
-                      -- PmenuSbar = { bg = theme.ui.bg_m1 },
-                      -- PmenuThumb = { bg = theme.ui.bg_p2 },
+                        Pmenu = { bg = theme.ui.bg_m3, fg = theme.ui.fg_dim },
+                        -- PmenuSel = { bg = theme.ui.bg_m1, fg = theme.ui.fg },
+                        -- PmenuSbar = { bg = theme.ui.bg_m1 },
+                        -- PmenuThumb = { bg = theme.ui.bg_p2 },
 
-                  }
-              end,
+                    }
+                end,
             })
 
             -- setup must be called before loading
