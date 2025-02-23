440ce078c6a47990e9703f4842045031c677a303
Enrique C <enricava@ucm.es>
2025-02-23 21:02:05 +0100

    wezterm: transparency

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,7 +8,7 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.adjust_window_size_when_changing_font_size = false
 
-config.window_background_opacity = 1.0
+config.window_background_opacity = .9
 
 config.window_padding = {
   left = 0,
