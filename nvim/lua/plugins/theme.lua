16032f200ab3f1e5ef5e15d57f27193da04588a6
Enrique C <enricava@ucm.es>
2025-02-23 21:00:38 +0100

    nvim: (theme) transparency

---
+++ nvim/lua/plugins/theme.lua
@@ -0,0 +1,13 @@
+return {
+  -- Enable transparency for tokyonight
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
