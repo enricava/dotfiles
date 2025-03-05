20c55a041c4d2de556df4298be0299279e243f81
Enrique C <enricava@ucm.es>
2025-03-05 22:41:47 +0100

    nvim: fix ssh copy&paste

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+local function set_ssh_clipboard()
+  if vim.env.SSH_CONNECTION ~= nil and vim.env.SSH_CONNECTION ~= "" then
+    vim.g.clipboard = {
+      name = "OSC 52",
+      copy = {
+        ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+        ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+      },
+      paste = {
+        ["+"] = function() end,
+        ["*"] = function() end,
+      },
+    }
+  end
+end
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  pattern = "*",
+  callback = set_ssh_clipboard,
+})
