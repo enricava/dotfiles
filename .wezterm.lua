80ddf7716649c8329a88185ce6fd5229ef7674dc
enricava <enricava@ucm.es>
2025-03-22 12:25:20 +0100

    wezterm: background opacity 1

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,5 @@
 -- Pull in the wezterm API
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
@@ -8,20 +8,20 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.adjust_window_size_when_changing_font_size = false
 
-config.window_background_opacity = .9
+config.window_background_opacity = 1
 
 config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
+	left = 0,
+	right = 0,
+	top = 0,
+	bottom = 0,
 }
 
 config.keys = {
-  {
-    key = 'F11',
-    action = wezterm.action.ToggleFullScreen,
-  },
+	{
+		key = "F11",
+		action = wezterm.action.ToggleFullScreen,
+	},
 }
 
 config.font = wezterm.font 'Iosevka Term'
