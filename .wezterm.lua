b691ef9643c4f1bf6007457f5c45de24fcf60914
enricava <enricava@ucm.es>
2025-03-24 20:57:49 +0100

    wezterm: remove window decorations

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,6 +10,8 @@ config.adjust_window_size_when_changing_font_size = false
 
 config.window_background_opacity = 1
 
+config.window_decorations = "RESIZE"
+
 config.window_padding = {
 	left = 0,
 	right = 0,
