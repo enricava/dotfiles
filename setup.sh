c72806d31f2c099521525be674b3caefafe8f401
enricava <enricava@ucm.es>
2025-03-26 23:22:00 +0100

    hx: add languages toml

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ ln -s ~/dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/dotfiles/.wezterm.lua ~/.wezterm.lua
 ln -s ~/dotfiles/yazi ~/.config/yazi
 ln -s ~/dotfiles/helix/config.toml ~/.config/helix/config.toml
+ln -s ~/dotfiles/helix/languages.toml ~/.config/helix/languages.toml
