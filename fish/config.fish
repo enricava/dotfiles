094d96a807169352d213878ffa88d23a4726ebb4
enricava <enricava@ucm.es>
2025-08-08 16:11:45 +0200

    fish: add macos llvm to path

--- fish/config.fish
+++ fish/config.fish
@@ -13,6 +13,10 @@ fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.pub-cache/bin
 fish_add_path $HOME/go/bin
 
+# MacOs Shenanigans
+fish_add_path /Library/Developer/CommandLineTools/usr/bin
+set -gx LIBRARY_PATH "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
+
 set -x N_PREFIX "$HOME/n"
 contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
 
