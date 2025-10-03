11acb72dd58417dd0a08d3ca34064301e232e6e4
enricava <enricava@ucm.es>
2025-10-03 11:17:49 +0200

    wezterm: add transparency

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,7 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.adjust_window_size_when_changing_font_size = false
 
-config.window_background_opacity = 1
+config.window_background_opacity = .9
 
 config.window_decorations = "RESIZE"
 
