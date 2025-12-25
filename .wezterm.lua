a2482f7d5a5fc2d8b332b27a1ab424be5533cf14
enricava <enricava@ucm.es>
2025-12-25 20:37:46 +0100

    wezterm: add blur

--- .wezterm.lua
+++ .wezterm.lua
@@ -10,7 +10,8 @@ config.hide_tab_bar_if_only_one_tab = true
 
 config.adjust_window_size_when_changing_font_size = false
 
-config.window_background_opacity = .9
+config.window_background_opacity = .6
+config.macos_window_background_blur = 80
 
 config.window_decorations = "RESIZE"
 
