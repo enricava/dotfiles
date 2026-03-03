a516b339f4e8cf1ac4540a9859cfb512b53e2b3a
enricava <enricava@ucm.es>
2026-03-03 20:17:27 +0100

    fish: update EDITOR variable

--- fish/config.fish
+++ fish/config.fish
@@ -28,7 +28,7 @@ set -x N_PREFIX "$HOME/n"
 contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
 
 if status is-interactive
-    set -gx EDITOR hx
+    set -gx EDITOR (command -v hx)
 
     set -gx WORKSPACE $HOME/repos
 
