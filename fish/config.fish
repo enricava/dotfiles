fba64706de0d112d398d163235d94e2ce7e9a773
enricava <enricava@ucm.es>
2026-03-03 20:20:24 +0100

    fish: cleanup config

--- fish/config.fish
+++ fish/config.fish
@@ -1,42 +1,34 @@
 set -gx GOPATH "$HOME/go"
 set -gx GOBIN "$HOME/go/bin"
 
-fish_add_path $HOME/.local/bin
-fish_add_path /opt/nvim-linux64/bin
-fish_add_path /usr/local/android-studio/bin
-fish_add_path $HOME/development/flutter/bin
-fish_add_path /usr/local/go/bin
 fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.pub-cache/bin
+fish_add_path $HOME/development/flutter/bin
 fish_add_path $HOME/go/bin
+fish_add_path /usr/local/android-studio/bin
+fish_add_path /usr/local/go/bin
+fish_add_path /opt/nvim-linux64/bin
+fish_add_path /opt/homebrew/bin/
+fish_add_path /opt/homebrew/opt/openjdk/bin
 fish_add_path /Applications/Obsidian.app/Contents/MacOS
 
-fish_add_path /opt/homebrew/bin/
-
-# Openjdk
-fish_add_path /opt/homebrew/opt/openjdk/bin
 set -gx CPPFLAGS -I/opt/homebrew/opt/openjdk/include
-
-# MacOs Shenanigans
-# fish_add_path /Library/Developer/CommandLineTools/usr/bin
-# set -gx LIBRARY_PATH "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/lib"
 
 set -x N_PREFIX "$HOME/n"
 contains "$N_PREFIX/bin" $PATH; or set -a PATH "$N_PREFIX/bin" # Added by n-install (see http://git.io/n-install-repo).
 
 if status is-interactive
+    # Helix config
     set -gx EDITOR (command -v hx)
+    set -gx NODE_MODULES_GLOBAL (npm -g root)
 
     set -gx WORKSPACE $HOME/repos
 
     alias l "ls -la"
     alias v nvim
-    alias vim nvim
 
     starship init fish | source
     fzf --fish | source
     nvm -s use 22.12.0
-
-    # Helix configuration for node modules
-    set -gx NODE_MODULES_GLOBAL (npm -g root)
 end
